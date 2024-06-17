3afd257efc5c1aa642aba1531173b4e1bd234cc7
Hariom Dhage <130223726+HariomDhage@users.noreply.github.com>
2024-06-18 00:28:24 +0530

    chore: Add login, logout, and token refresh functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,27 @@ import { application } from "express";
 import { uploadOnCloudinary } from "../utils/Cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import {User} from "../models/user_model.js";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+
+const generateAccessAndRefereshTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "Something went wrong while generating referesh and access token"
+    );
+  }
+};
+
 
 const registerUser = asyncHandler(async (req, res) => {
   // get user details from frontend
@@ -17,7 +38,7 @@ const registerUser = asyncHandler(async (req, res) => {
   // return res
 
   const { fullName, email, username, password } = req.body;
-  console.log("email: ", email);
+ // console.log("email: ", email);
 
   if (
     [fullName, email, username, password].some((field) => field?.trim() === "")
@@ -76,6 +97,146 @@ const registerUser = asyncHandler(async (req, res) => {
   //  if (fullName === "") {
   //     throw new ApiError(400, "fullname is requaried!!")
   //  }
-});
+})
 
-export { registerUser };
+
+const loginUser = asyncHandler(async (req, res) => {
+  // req body -> data
+  // username or email se validate
+  // find  the user in db
+  // check password
+  // access and refresh token provide to user
+  // send cookie
+  // return res
+
+  const { email,username, password } = req.body;
+
+  if (!email && !username) {
+    throw new ApiError(400, "username or email is required")
+  }
+
+  //here if we want altrenative apporoach then
+  //  if (!(email || username)) {
+  //    throw new ApiError(400, "username or email is required");
+  //  }
+
+  const user = await User.findOne({
+    $or: [{username}, {email}]
+  })
+
+  if (!user) {
+    throw new ApiError(400, "user does not exist")
+  }
+
+  const isPasswordValid =await user.isPasswordCorrect(password)
+  if (!isPasswordValid) {
+    throw new ApiError(401,"invalid user credentials!!")
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefereshTokens(
+    user._id
+  );
+
+  const loggedInUsser = await User.findById(user._id).select("-password -refreshToken")
+
+  const options = {
+    httpOnly: true,
+    secure: true
+  }
+
+  return res
+  .status(200)
+  .cookie("accessToken",accessToken,options)
+  .cookie("refreshToken",refreshToken,options)
+  .json(
+    new ApiResponse(
+      200,
+      {
+        user: loggedInUsser, accessToken, refreshToken
+      },
+      "User logged In Successfully"
+    )
+  )
+
+
+})
+
+
+const logoutUser = asyncHandler(async(req,res) => {
+  User.findByIdAndUpdate(
+    req.user._id, 
+    {
+      $set:{
+        refreshToken: undefined
+      }
+
+    },
+    {
+      new: true
+    }
+  )
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  }
+
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {} , "User logged Out"))
+})
+
+const refreshAccessToken = asyncHandler(async(req,res)=>{
+
+ const incomingRefreshToken = req.cookie.refreshToken || req.body.refreshToken
+
+ if (incomingRefreshToken) {
+
+  throw new ApiError(401,"unauthorise access")
+ }
+
+ try {
+  const decodedToken = jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET)
+ 
+  const user = User.findById(decodedToken?._id)
+ 
+  if (!user) {
+    throw new ApiError(401, "invalid refresh token");
+  }
+ 
+  if (incomingRefreshToken !== user?.refreshToken) {
+   throw new ApiError(401, "refresh token expaired");
+  
+  }
+ 
+  const options = {
+   httpOnly: true ,
+   secure:true
+  }
+ 
+ const {accessToken, newRefreshToken} =await generateAccessAndRefereshTokens(user?._id)
+ 
+ return res
+ .status(200)
+ .cookie("accessToken",accessToken,options)
+ .cookie("refreshToken",newRefreshToken,options)
+ .json(
+   
+     new ApiResponse(
+       200,
+       {accessToken,
+         refreshToken: newRefreshToken},
+         "access token refreshed successfully"
+       
+     )
+   
+ )
+ } catch (error) {
+  throw new ApiError(401, error?.message || "Invalid refresh token")
+ }
+})
+
+
+export { registerUser, loginUser, logoutUser, refreshAccessToken };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,12 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+  refreshAccessToken,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middlewares.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
@@ -17,6 +23,14 @@ const router = Router()
     ]),
     registerUser
 )
+
+router.route("/login").post(loginUser)
+
+// secure routes
+router.route("/logout").post(verifyJWT ,logoutUser)
+
+router.route("/refresh-token").post(refreshAccessToken)
+
 
 
 export default router
